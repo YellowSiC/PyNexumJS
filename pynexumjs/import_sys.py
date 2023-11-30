f2720a29c41b2d8ba02f0003b48b4c81bd9f0355
Ari Bermeki <90009669+AriBermeki@users.noreply.github.com>
2023-11-30 16:33:43 +0100

    Update import_sys.py

--- pynexumjs/import_sys.py
+++ pynexumjs/import_sys.py
@@ -1,6 +1,6 @@
 from .JsPyFunction import  expose, callable, exclusive_callable, clear,is_running, get_keys, has, call_nowait, call
 from .JsPyPubsub import subscribe, unsubscribe, publish
 from .JsPyQueue import push_nowait, push, pop, shift, add_callback,clear_callback, is_empty, get_keys,has, clear, clear_all, remove, remove_all
-from .electron import BrowserModule
+from .electron import Application
 from .JsPyBinarySocket import JsPyBinarySocket
 from .socket_mennager import WebSocketManager
